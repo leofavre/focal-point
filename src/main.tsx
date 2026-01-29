c37d1a65da49b6df98e1d84292cbfceb27978abe
leofavre <leo@leofavre.com>
2026-01-29 15:23:42 -0300

    main.css comes from the public folder now

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./style.css";
 import { initDB } from "react-indexed-db-hook";
+import App from "./App";
 
 const rootElement = document.getElementById("app");
 
